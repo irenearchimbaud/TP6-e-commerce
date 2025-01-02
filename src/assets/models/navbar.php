a3990ef942b431202ba4b0970b575162c6c58b75
Emree66 <emre.sanal@ecole-isitech.fr>
2025-01-02 16:56:58 +0100

    modif entiereté navbar avec meilleure responsive

--- src/assets/models/navbar.php
+++ src/assets/models/navbar.php
@@ -1,16 +1,156 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../css/navbar.css">
+    <title>Chaussures Market</title>
 </head>
+
 <body>
-    <nav>
-        <a href="#" class="active">Accueil</a>
-        <a href="#produits">Produits</a>
-        <a href="#priseDeCommande">Prise De Commande</a>
-        <a href="#contact">Contact</a>
-        <a href="#aPropos">A propos</a>
-        <a href="#login-signup">Se connecter - S'Inscrire</a>
-    </nav>
+    <header>
+        <div class="container">
+            <input type="checkbox" name="" id="check">
+            
+            <div class="logo-container">
+                <h3 class="logo">Chaussures<span> Market</span></h3>
+            </div>
+
+            <div class="nav-btn">
+                <div class="nav-links">
+                    <ul>
+                        <li class="nav-link" style="--i: .6s">
+                            <a href="#">Acceuil</a>
+                        </li>
+                        <li class="nav-link" style="--i: .85s">
+                            <a href="#">Catégories<i class="fas fa-caret-down"></i></a>
+                            <div class="dropdown">
+                                <ul>
+                                    <li class="dropdown-link">
+                                        <a href="#">Nike</a>
+                                    </li>
+                                    <li class="dropdown-link">
+                                        <a href="#">Adidas</a>
+                                    </li>
+                                    <li class="dropdown-link">
+                                        <a href="#">New Balance<i class="fas fa-caret-down"></i></a>
+                                        <div class="dropdown second">
+                                            <ul>
+                                                <li class="dropdown-link">
+                                                    <a href="#">NB 740</a>
+                                                </li>
+                                                <li class="dropdown-link">
+                                                    <a href="#">NB 9060</a>
+                                                </li>
+                                                <li class="dropdown-link">
+                                                    <a href="#">NB 574</a>
+                                                </li>
+                                                <li class="dropdown-link">
+                                                    <a href="#">More<i class="fas fa-caret-down"></i></a>
+                                                    <div class="dropdown second">
+                                                        <ul>
+                                                            <li class="dropdown-link">
+                                                                <a href="#">NB 1000</a>
+                                                            </li>
+                                                            <li class="dropdown-link">
+                                                                <a href="#">NB 2080</a>
+                                                            </li>
+                                                            <li class="dropdown-link">
+                                                                <a href="#">NB 220</a>
+                                                            </li>
+                                                            <div class="arrow"></div>
+                                                        </ul>
+                                                    </div>
+                                                </li>
+                                                <div class="arrow"></div>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                    <li class="dropdown-link">
+                                        <a href="#">Puma</a>
+                                    </li>
+                                    <div class="arrow"></div>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="nav-link" style="--i: 1.1s">
+                            <a href="#">Services<i class="fas fa-caret-down"></i></a>
+                            <div class="dropdown">
+                                <ul>
+                                    <li class="dropdown-link">
+                                        <a href="#">Nettoyage</a>
+                                    </li>
+                                    <li class="dropdown-link">
+                                        <a href="#">Réparation</a>
+                                    </li>
+                                    <li class="dropdown-link">
+                                        <a href="#">Personnalisation<i class="fas fa-caret-down"></i></a>
+                                        <div class="dropdown second">
+                                            <ul>
+                                                <li class="dropdown-link">
+                                                    <a href="#">Couleurs</a>
+                                                </li>
+                                                <li class="dropdown-link">
+                                                    <a href="#">Matériaux</a>
+                                                </li>
+                                                <li class="dropdown-link">
+                                                    <a href="#">Design</a>
+                                                </li>
+                                                <li class="dropdown-link">
+                                                    <a href="#">Plus<i class="fas fa-caret-down"></i></a>
+                                                    <div class="dropdown second">
+                                                        <ul>
+                                                            <li class="dropdown-link">
+                                                                <a href="#">Lacets</a>
+                                                            </li>
+                                                            <li class="dropdown-link">
+                                                                <a href="#">Semelles</a>
+                                                            </li>
+                                                            <li class="dropdown-link">
+                                                                <a href="#">Accessoires</a>
+                                                            </li>
+                                                            <div class="arrow"></div>
+                                                        </ul>
+                                                    </div>
+                                                </li>
+                                                <div class="arrow"></div>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                    <li class="dropdown-link">
+                                        <a href="#">Consultation</a>
+                                    </li>
+                                    <div class="arrow"></div>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="nav-link" style="--i: 1.35s">
+                            <a href="#">A Propos</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="log-sign" style="--i: 1.8s">
+                    <a href="#" class="btn transparent">Log in</a>
+                    <a href="#" class="btn solid">Sign up</a>
+                </div>
+            </div>
+
+            <div class="hamburger-menu-container">
+                <div class="hamburger-menu">
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <main>
+        <section>
+            <div class="overlay"></div>
+        </section>
+    </main>
 </body>
+
 </html>
