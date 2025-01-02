d2674aa459d93dc183c19261006b60462fc8eb5e
NayirS <nadow692@gmail.com>
2025-01-02 15:49:48 +0100

    ajout pages accueil

---
+++ src/views/home/home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<head>
+    <title>Mon Site Web</title>
+    <link rel="stylesheet" href="HomeStyles.css">
+</head>
+<body>
+
+    <div class="header">
+        <h1>Les ChaussGit</h1> <
+    </div>
+
+    <div class="button-container">
+        
+        <div class="Login">
+            <button class="login-Bouton" type="submit">Login</button>
+        </div>
+        
+        <
+        <div class="Création de compte">
+            <button class="Création-button" type="submit">Création</button>
+        </div>
+        
+        
+        <div class="Voir Produit">
+            <button class="Produit-Bouton" type="submit">Produit</button>
+        </div>
+    </div>
+</body>
+</html>
+
