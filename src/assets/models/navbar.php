93f584012a4884e3ca110184909e35756637ad1d
Emree66 <emre.sanal@ecole-isitech.fr>
2025-01-02 15:17:18 +0100

    Modif navbar pour forcer position fixe + Ajout fichier css navbar.css pour mise en forme

--- src/assets/models/navbar.php
+++ src/assets/models/navbar.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Template Menu</title>
+    <title>Document</title>
 </head>
 <body>
     <nav>
-        <a href="#">Accueil</a>
+        <a href="#" class="active">Accueil</a>
         <a href="#produits">Produits</a>
         <a href="#priseDeCommande">Prise De Commande</a>
         <a href="#contact">Contact</a>
