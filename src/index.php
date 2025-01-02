8cbd997505518d8c33286b247b92ae93693e7662
NayirS <nadow692@gmail.com>
2025-01-02 16:24:25 +0100

    changement dans index et majuscule et accent enlever

--- src/index.php
+++ src/index.php
@@ -8,7 +8,7 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
-    header("Location: views/home/home.php");
+    header("Location: home.php");
     exit; 
     
 } catch (PDOException $e) {
